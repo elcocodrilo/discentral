60bd7b34c30db742bd8a053fea734717208be729
Taylor <taylor@blockchaintechltd.com>
2015-10-26 21:21:15 -0700

    Improve startup log

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # bundle js
+echo 'Bundling the javascript,'
 browserify -t icsify -o lib/frontend/public/bundle.js lib/frontend/scripts/main.iced;
 # svg to javascript object
+echo 'javascripting the svg'
 ./lib/frontend/svgs/svgToJs.sh lib/frontend/svgs/bg.svg;
 # bundle css
 gulp;
